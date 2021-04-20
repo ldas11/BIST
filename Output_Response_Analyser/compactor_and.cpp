ca3622ed7cab9d1ab3e42ded472153c75593f412
Luis <urdjt@student.kit.edu>
2021-04-20 22:27:54 +0200

    fixed a problem with the bits being compacted

--- Output_Response_Analyser/compactor_and.cpp
+++ Output_Response_Analyser/compactor_and.cpp
@@ -5,14 +5,14 @@ void compactor_nand::compact() {
 	toCompact_temp = toCompact_in.read();
 	compacted_temp = 1;
 	
-	for (int i = 0; i < 29; i++) {
+	for (int i = 0; i < 30; i++) {
 		compacted_temp[0] &= (toCompact_temp[i] & toCompact_temp[i + 1]);
 	}
 	
-	if (compacted_temp[0] == 1) {
+	if (compacted_temp[0] == 0b1) {
 		compacted_out.write(1);
 	}
-	else if(compacted_temp[0] == 0)
+	else if(compacted_temp[0] == 0b0)
 	{
 		compacted_out.write(0);
 	}
